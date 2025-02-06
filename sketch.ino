9a218b940186066686e90551b5871a183f2f5050
LilyHinde <S00268079@atu.ie>
2025-02-06 14:35:47 +0000

    sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,7 +1,9 @@
 // the setup routine runs once when you press reset:
+int LED = 2;
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  pinMode(LED, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
@@ -10,5 +12,14 @@ void loop() {
   int knobValue = analogRead(A0);
    // print out the value you read:
   Serial.println(knobValue);
+ if (knobValue > 400)
+  {
+    digitalWrite(LED, HIGH); //turn LED on
+  }
+  else
+  {
+    digitalWrite(LED, LOW); //Turn LED off, keep it off
+  }
 
+  delay(100); //small delay in between reads for stability
 }
